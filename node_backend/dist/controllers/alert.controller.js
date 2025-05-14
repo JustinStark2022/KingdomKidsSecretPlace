1e7621caab8106459f0b342174d87c5d5f5cf8ac
JustinStark2022 <starkjustin87@gmail.com>
2025-05-14 14:45:55 -0500

    fixed container tobiuld and run the app

--- node_backend/dist/controllers/alert.controller.js
+++
@@ -1,6 +0,0 @@
-export const getAlerts = async (_req, res) => {
-    res.json([
-        { id: 1, name: "Game A", platform: "Roblox", flagReason: "violence", contentType: "game" },
-        { id: 2, name: "Video B", platform: "YouTube", flagReason: "language", contentType: "video" }
-    ]);
-};
--- node_backend/dist/routes/alerts.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { getAlerts } from "@/controllers/alert.controller";
-import { verifyToken } from "@/middleware/auth.middleware";
-const router = Router();
-router.get("/alerts/recent", verifyToken, getAlerts);
-export default router;
